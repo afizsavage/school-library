80cca974a7019518c945b8b09b57c6c1741ab77f
Afiz Savage <savageafiz9@gmail.com>
2022-03-16 21:23:22 +0000

    Created student sub class

--- person.rb
+++ person.rb
@@ -1,4 +1,8 @@
 class Person
+  attr_reader :id
+
+  attr_accessor :name, :age
+
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1...1000)
     @name = name
@@ -6,12 +10,8 @@ class Person
     @parent_permission = parent_permission
   end
 
-  attr_reader :id
-
-  attr_accessor :name, :age
-
   def can_use_services?(_age, _parent_permission)
-    true if is_of_age || @parent_permission == true
+    true if of_age || @parent_permission == true
   end
 
   private
