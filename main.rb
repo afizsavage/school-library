39a255f6dd379a7e2983f478991b23f855a7ae4b
Afiz Savage <savageafiz9@gmail.com>
2022-03-19 13:29:04 +0000

    Created main entry point and relating methods for the app

--- main.rb
+++ main.rb
@@ -1,10 +1,35 @@
-require './person'
-require './capitalized'
-require './trimmer'
+#!/usr/bin/env ruby
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = Capitalized.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = Trimmer.new(capitalized_person)
-capitalized_trimmed_person.correct_name
+class MainEntryPoint
+  def initialize
+    @option = nil
+  end
+
+  def print_options
+    puts
+    puts ['1 - List all books', '2 - List all people', '3 - Create a person', '4 - Create a book',
+          '5 - Create a rental', '6 - List all rentals for a given person', '7 - Exit']
+  end
+
+  def choose_options
+    @option = gets.chomp
+  end
+
+  def process_option
+    @option == 7 ? exit : @option
+  end
+
+  def run
+    puts 'Welcome to School Library App!'
+    print_options
+    choose_options
+    process_option
+  end
+end
+
+def main
+  app = MainEntryPoint.new
+  app.run
+end
+
+main
