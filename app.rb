45cbd099c9448d38009b0db4554f014a678dc8af
Afiz Savage <savageafiz9@gmail.com>
2022-03-19 16:31:01 +0000

    Added method to add new students

--- app.rb
+++ app.rb
@@ -8,13 +8,26 @@ class App
   end
 
   def create_person
-    print 'Do you want to create a student(1) a teacher(2)? [Input the number]:'
+    print 'Do you want to create a student(1) a teacher(2)? [Input tNhe number]:'
     option = gets.chomp
 
     case option
-    when 1
-      puts 'create student'
-    when 2
+    when '1'
+      print 'Age '
+      age = gets.chomp
+
+      print 'Name '
+      name = gets.chomp
+
+      print 'Has parent permission? [Y/N]: '
+      has_permission = gets.chomp
+      parent_permission = has_permission.include? 'Yy'
+      classroom = "Room 1"
+
+      @people.push(Student.new(age, name, parent_permission))
+
+      puts 'New Teacher created successfully'
+    when '2'
       puts 'create teacher'
     end
   end
