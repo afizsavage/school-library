1847eb6d39b3188d0614d3c5263a660ff9b4bfa5
blessedjasonmwanza <mwanzabj@gmail.com>
2022-03-25 11:33:06 +0200

    Test person class

---
+++ __tests__/person.rb
@@ -0,0 +1,18 @@
+require_relative '../person'
+require_relative '../nameable'
+
+describe Person do
+  context 'Test the Person class' do
+    person = Person.new(50, 'Blessed Jason Mwanza', parent_permission: true)
+
+    it 'has class Person' do
+      expect(person.class) == Person
+    end
+
+    it 'must the right name, age and permission' do
+      expect(person.age).to eq 50
+      expect(person.name).to eq 'Blessed Jason Mwanza'
+      expect(person.parent_permission).to eq true
+    end
+  end
+end
