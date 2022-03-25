374b3c14108d4561cbbf17a332b70d12b60bd866
blessedjasonmwanza <mwanzabj@gmail.com>
2022-03-25 13:20:10 +0200

    Cover all test coverages

---
+++ __tests__/teacher.rb
@@ -0,0 +1,22 @@
+require_relative '../teacher'
+
+describe Teacher do
+  context 'Testing the Teacher class' do
+    teacher = Teacher.new(60, 'Math', 'teacher1')
+
+    it 'has the class of Teacher' do
+      expect(teacher.instance_of?(Teacher)) == true
+    end
+
+    it 'should have the right name - teacher1, age = 60 and specialization - math' do
+      expect(teacher.age).to eq 60
+      expect(teacher.name).to eq 'teacher1'
+      expect(teacher.specialization).to eq 'Math'
+    end
+
+    it 'should be able to use services' do
+      expect(teacher.can_use_services?).to eq true
+    end
+  end
+end
+o
